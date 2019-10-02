6ccc761a1231d07b32e5a389caa8a2d26ea1719b
jlf44 <jlf44@cam.ac.uk>
2019-10-02 13:08:12 +0100

    Add security

---
+++ src/main/java/uk/cam/lib/cdl/loading/security/BasicWebSecurityConfigurerAdapter.java
@@ -0,0 +1,55 @@
+package uk.cam.lib.cdl.loading.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class BasicWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Value("${dl-loading-ui.basic.username}")
+    private String basicUsername;
+
+    @Value("${dl-loading-ui.basic.password}")
+    private String basicPassword;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser(basicUsername).password(passwordEncoder().encode(basicPassword))
+            .authorities("ROLE_USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/js").permitAll()
+            .antMatchers("/css").permitAll()
+            .antMatchers("/img").permitAll()
+            .antMatchers("/login/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic()
+            .authenticationEntryPoint(authenticationEntryPoint);
+
+        http.addFilterAfter(new CustomFilter(),
+            BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
