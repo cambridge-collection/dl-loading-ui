641e4f081f646c3a62b7ca7f5ecec836737c55ba
Hal Blackburn <hwtb2@cam.ac.uk>
2020-03-11 08:26:28 +0000

    Support configuring a Spring Security PasswordEncoder

---
+++ src/main/java/uk/cam/lib/cdl/loading/security/PasswordEncodingConfig.java
@@ -0,0 +1,36 @@
+package uk.cam.lib.cdl.loading.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncodingConfig {
+
+    private static final class NoopPasswordEncoder implements PasswordEncoder {
+        @Override
+        public String encode(CharSequence rawPassword) {
+            return "" + rawPassword;
+        }
+
+        @Override
+        public boolean matches(CharSequence rawPassword, String encodedPassword) {
+            return rawPassword.equals(encodedPassword);
+        }
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "auth.password-encoding.method", havingValue = "insecure-plaintext-for-testing")
+    public PasswordEncoder plaintextNoopPasswordEncoder() {
+        return new NoopPasswordEncoder();
+
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "auth.password-encoding.method", havingValue = "default", matchIfMissing = true)
+    public PasswordEncoder defaultPasswordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
