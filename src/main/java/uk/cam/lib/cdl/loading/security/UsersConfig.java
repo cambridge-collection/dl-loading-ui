68e72c867f6fafbf2d171c49bef06d5018621b36
jlf44 <jlf44@cam.ac.uk>
2020-03-25 20:22:38 +0000

    Updating Users Config to have separate 'hardcoded' and 'database' options

--- src/main/java/uk/cam/lib/cdl/loading/security/UsersConfig.java
+++ src/main/java/uk/cam/lib/cdl/loading/security/UsersConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -38,6 +39,11 @@ public class UsersConfig {
     @ConditionalOnExpression("@environment.getProperty('users.source') == 'hardcoded' || !@environment.containsProperty('users.source')")
     public @interface ConditionalOnHardcodedUsersSource { }
 
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @ConditionalOnExpression("@environment.getProperty('users.source') == 'database'")
+    public @interface ConditionalOnDatabaseUsersSource { }
+
     @ConditionalOnHardcodedUsersSource
     public static class HardcodedUsersConfig {
         private static final Logger LOG = LoggerFactory.getLogger(HardcodedUsersConfig.class);
@@ -57,15 +63,16 @@ public class UsersConfig {
         @Lazy
         public Map<String, UserDetails> hardcodedUsers(
             @Qualifier("hardcodedUsersProperties") Properties hardcodedUsersProperties,
-            @Qualifier(QUALIFIER + "#userDetailsService") UserDetailsService userDetailsService,
             PasswordEncoder passwordEncoder) {
+            // Use InMemoryUserDetailsManager to parse the user properties into user objects
+            var udm = new InMemoryUserDetailsManager(hardcodedUsersProperties);
 
             return hardcodedUsersProperties.keySet().stream().map(username -> (String)username).map(username -> {
                 if(!username.toLowerCase().equals(username)) {
                     throw new IllegalArgumentException("Hardcoded usernames must be lower case; got: " + username);
                 }
 
-                UserDetails user = userDetailsService.loadUserByUsername(username);
+                var user = udm.loadUserByUsername(username);
 
                 if(passwordEncoder.upgradeEncoding(user.getPassword())) {
                     user = new User(
@@ -78,9 +85,21 @@ public class UsersConfig {
             }).collect(ImmutableMap.toImmutableMap(UserDetails::getUsername, u -> u));
         }
 
-        @Bean(name = {QUALIFIER+"#DBUserDetailsService", QUALIFIER})
-        public UserDetailsService dbUserDetailsService() {
-            return new DBUserDetailsService();
+        /**
+         * The UserDetailsService for authn/authz - backed by a fixed list of users from a config file.
+         */
+        @Bean(name = {QUALIFIER + "#userDetailsManager", QUALIFIER})
+        @ConditionalOnExpression("!(@environment.getProperty('users.hardcoded-users-file', '').isEmpty())")
+        @Qualifier(QUALIFIER)
+        public InMemoryUserDetailsManager userDetailsManager(@Qualifier(HARDCODED_USERS) Map<String, UserDetails> hardcodedUsers) {
+            return new InMemoryUserDetailsManager(hardcodedUsers.values());
+        }
+
+        @Bean(name = {QUALIFIER + "#emptyUserDetailsManager", QUALIFIER})
+        @ConditionalOnExpression("@environment.getProperty('users.hardcoded-users-file', '') == ''")
+        public InMemoryUserDetailsManager emptyUserDetailsManager() {
+            LOG.warn("Property users.hardcoded-users-file not set, no users will be defined");
+            return new InMemoryUserDetailsManager();
         }
 
         @Component
@@ -89,24 +108,54 @@ public class UsersConfig {
         public static class HardcodedUsersAuthSecurityConfigurer
             extends SecurityConfigurerAdapter<AuthenticationManager, AuthenticationManagerBuilder> {
 
+            private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
             private final PasswordEncoder passwordEncoder;
-            private final UserDetailsService userDetailsService;
 
             public HardcodedUsersAuthSecurityConfigurer(
-                @Qualifier(QUALIFIER + "#DBUserDetailsService") UserDetailsService userDetailsService,
+                @Qualifier(QUALIFIER) InMemoryUserDetailsManager inMemoryUserDetailsManager,
                 PasswordEncoder passwordEncoder
             ) {
+                this.inMemoryUserDetailsManager = checkNotNull(inMemoryUserDetailsManager);
                 this.passwordEncoder = checkNotNull(passwordEncoder);
+            }
+
+            @Override
+            public void configure(AuthenticationManagerBuilder auth) throws Exception {
+                auth.userDetailsService(this.inMemoryUserDetailsManager)
+                    .passwordEncoder(this.passwordEncoder);
+            }
+        }
+    }
+
+    @ConditionalOnDatabaseUsersSource
+    public static class DatabaseUsersConfig {
+        private static final Logger LOG = LoggerFactory.getLogger(DatabaseUsersConfig.class);
+        private static final String QUALIFIER = "uk.cam.lib.cdl.loading.security.UsersConfig.DatabaseUsersConfig";
+
+        @Bean(name = {QUALIFIER+"#DBUserDetailsService", QUALIFIER})
+        public UserDetailsService dbUserDetailsService() {
+            return new DBUserDetailsService();
+        }
+
+        @Component
+        @ConditionalOnDatabaseUsersSource
+        @Qualifier(WebSecurityConfig.QUALIFIER_AUTH_SUB_CONFIGURER)
+        public static class DatabaseUsersAuthSecurityConfigurer
+            extends SecurityConfigurerAdapter<AuthenticationManager, AuthenticationManagerBuilder> {
+
+            private final UserDetailsService userDetailsService;
+
+            public DatabaseUsersAuthSecurityConfigurer(
+                @Qualifier(QUALIFIER + "#DBUserDetailsService") UserDetailsService userDetailsService
+            ) {
                 this.userDetailsService = userDetailsService;
             }
 
             @Override
             public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-                auth.userDetailsService(this.userDetailsService)
-                    .passwordEncoder(this.passwordEncoder);
+                auth.userDetailsService(this.userDetailsService);
             }
         }
     }
-
 }
