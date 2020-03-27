13b4571fe9bcb2180b938917c66ff8ac02582f67
Hal Blackburn <hwtb2@cam.ac.uk>
2020-03-27 08:32:25 +0000

    Define constructor for DBUserDetailsService

--- src/main/java/uk/cam/lib/cdl/loading/security/DBUserDetailsService.java
+++ src/main/java/uk/cam/lib/cdl/loading/security/DBUserDetailsService.java
@@ -1,15 +1,18 @@
 package uk.cam.lib.cdl.loading.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import uk.cam.lib.cdl.loading.dao.UserRepository;
 import uk.cam.lib.cdl.loading.model.security.User;
 
-public class DBUserDetailsService implements UserDetailsService {
+import static com.google.common.base.Preconditions.checkNotNull;
 
-    @Autowired
-    private UserRepository userRepository;
+public class DBUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public DBUserDetailsService(UserRepository userRepository) {
+        this.userRepository = checkNotNull(userRepository, "userRepository was null");
+    }
 
     @Override
     public MyUserDetails loadUserByUsername(String username) {
