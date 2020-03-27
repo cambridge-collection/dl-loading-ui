1c630f8d0616701badfed5e5905550602842d3bc
Hal Blackburn <hwtb2@cam.ac.uk>
2020-03-27 10:26:38 +0000

    Add tests for SAMLUserDetailsServiceImpl

--- src/main/java/uk/cam/lib/cdl/loading/security/saml/SAMLUserDetailsServiceImpl.java
+++ src/main/java/uk/cam/lib/cdl/loading/security/saml/SAMLUserDetailsServiceImpl.java
@@ -1,60 +1,56 @@
 package uk.cam.lib.cdl.loading.security.saml;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.saml.SAMLCredential;
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
-import org.springframework.stereotype.Service;
-import uk.cam.lib.cdl.loading.dao.UserRepository;
-import uk.cam.lib.cdl.loading.model.security.User;
-import uk.cam.lib.cdl.loading.security.MyUserDetails;
+import uk.cam.lib.cdl.loading.security.UsersConfig;
 
-@Service
-@ConditionalOnSAMLAuth
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class SAMLUserDetailsServiceImpl implements SAMLUserDetailsService {
 
-    @Value("${auth.saml.attr.firstName}")
-    private String attrFirstName;
+    public static final String DEFAULT_FIRST_NAME_ATTRIBUTE = "urn:oid:2.5.4.42";
+    public static final String DEFAULT_LAST_NAME_ATTRIBUTE = "urn:oid:2.5.4.4";
+    public static final String DEFAULT_EMAIL_ATTRIBUTE = "urn:oid:1.2.840.113549.1.9.1";
 
-    @Value("${auth.saml.attr.lastName}")
-    private String attrLastName;
+    private final UserDetailsService userDetailsService;
+    private final String attrFirstName;
+    private final String attrEmail;
+    private final String attrLastName;
 
-    @Value("${auth.saml.attr.email}")
-    private String attrEmail;
+    public SAMLUserDetailsServiceImpl(UserDetailsService userDetailsService) {
+        this(userDetailsService, DEFAULT_FIRST_NAME_ATTRIBUTE, DEFAULT_LAST_NAME_ATTRIBUTE, DEFAULT_EMAIL_ATTRIBUTE);
+    }
 
-    @Autowired
-    private UserRepository userRepository;
+    public SAMLUserDetailsServiceImpl(
+            @Qualifier(UsersConfig.USER_DETAILS_SERVICE)
+            UserDetailsService userDetailsService,
+            String firstNameAttribute,
+            String lastNameAttribute,
+            String emailAttribute
+    ) {
+        this.userDetailsService = checkNotNull(userDetailsService, "userDetailsService was null");
+        this.attrFirstName = checkNotNull(firstNameAttribute, "firstNameAttribute was null");
+        this.attrLastName = checkNotNull(lastNameAttribute, "lastNameAttribute was null");
+        this.attrEmail = checkNotNull(emailAttribute, "emailAttribute was null");
+    }
 
     public UserDetails loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
 
         String userID = credential.getNameID().getValue();
 
-        /* // Not currently using other attributes values.
+        /*
+        // Not currently using other attributes values.
+        // Could use these to populate user info when auto-creating users on first login.
         String givenName = credential.getAttributeAsString(attrFirstName);
         String surname = credential.getAttributeAsString(attrLastName);
         String email = credential.getAttributeAsString(attrEmail);
         */
 
-        UserDetails userDetails = null;
-        try {
-            userDetails = loadUserByUsername(userID);
-
-        } catch (UsernameNotFoundException e) {
-            throw e;
-            // TODO fix deny access nicely
-        }
-
-        return userDetails;
-    }
-
-    private MyUserDetails loadUserByUsername(String username) {
-        User user = userRepository.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException(username);
-        }
-        return new MyUserDetails(user);
+        return checkNotNull(this.userDetailsService.loadUserByUsername(userID));
     }
 }
 
