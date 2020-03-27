e9dc704a04f123034382e8fb67dde2374fc3026c
Hal Blackburn <hwtb2@cam.ac.uk>
2020-03-27 08:30:08 +0000

    Fix duplicate UserDetailsService bean definition
    DBUserDetailsService should not use @Service to declare itself as a
    bean, as its bean definition is in the conditional DatabaseUsersConfig.
    If @Service is used then a bean instance is created unconditionally.

--- src/main/java/uk/cam/lib/cdl/loading/security/DBUserDetailsService.java
+++ src/main/java/uk/cam/lib/cdl/loading/security/DBUserDetailsService.java
@@ -3,11 +3,9 @@ package uk.cam.lib.cdl.loading.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 import uk.cam.lib.cdl.loading.dao.UserRepository;
 import uk.cam.lib.cdl.loading.model.security.User;
 
-@Service
 public class DBUserDetailsService implements UserDetailsService {
 
     @Autowired
