b648e6f3e42d296cae081bf3b2bf32a7ccf1b676
Hal Blackburn <hwtb2@cam.ac.uk>
2020-05-17 15:31:36 +0100

    Start simplifying Item model
    It doesn't need to know about the absolute path to the item file and
    it's simpler if it doesn't.

--- src/main/java/uk/cam/lib/cdl/loading/model/editor/Item.java
+++ src/main/java/uk/cam/lib/cdl/loading/model/editor/Item.java
@@ -1,34 +1,60 @@
 package uk.cam.lib.cdl.loading.model.editor;
 
 import com.google.common.base.Preconditions;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.lang.Nullable;
 
 import java.nio.file.Path;
+
+import static uk.cam.lib.cdl.loading.model.editor.ModelOps.ModelOps;
 
 /**
  * Not used for Jackson serialisation
  */
 public class Item {
+    private final Path id;
+    @Nullable
+    private final Path absolutePath;
 
-    private final String name;
-    private final Path filepath;
-    private final Id id;
+    public Item(Path id) {
+        this.id = ModelOps().validatePathForId(id);
+        this.absolutePath = null;
+    }
 
+    /**
+     * Deprecated in favor of holding only the item ID in the model. The
+     * absolute path is not part of the Item's state, rather is defined by the
+     * data directory it was loaded from.
+     */
+    @Deprecated
     public Item(String name, Path filepath, Id id) {
-        Preconditions.checkArgument(filepath.isAbsolute(), "filepath is not absolute: %s", filepath);
-        this.filepath = filepath.normalize();
-        this.name = name;
-        this.id = id;
+        Preconditions.checkNotNull(name);
+        Preconditions.checkNotNull(filepath);
+        Preconditions.checkNotNull(id);
+
+        this.id = ModelOps().validatePathForId(Path.of(id.getId()));
+        this.absolutePath = ModelOps().validatePathForIO(filepath);
     }
 
+    public Path getIdAsPath() {
+        return this.id;
+    }
+
+    @Deprecated
     public String getName() {
-        return name;
+        return FilenameUtils.getBaseName(id.getFileName().toString());
     }
 
+    @Deprecated
     public Path getFilepath() {
-        return filepath;
+        if(absolutePath == null) {
+            throw new UnsupportedOperationException("getFilepath() called on Item constructed with ID-only constructor");
+        }
+        return absolutePath;
     }
 
+    @Deprecated
     public Id getId() {
-        return id;
+        return new Id(this.id.toString());
     }
 }
