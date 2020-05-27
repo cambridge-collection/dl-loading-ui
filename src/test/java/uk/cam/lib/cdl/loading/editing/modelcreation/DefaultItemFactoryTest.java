f068457371de5fcb7118b72c7d5bf91710885327
Hal Blackburn <hwtb2@cam.ac.uk>
2020-05-27 12:44:58 +0100

    Remove unused test class

--- src/test/java/uk/cam/lib/cdl/loading/editing/modelcreation/DefaultItemFactoryTest.java
+++
@@ -1,9 +0,0 @@
-package uk.cam.lib.cdl.loading.editing.modelcreation;
-
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth8.assertThat;
-
-public class DefaultItemFactoryTest {
-
-
-}
