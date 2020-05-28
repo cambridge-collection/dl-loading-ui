8678dc15b0ba4533eb3a012c262ef864289f917a
Hal Blackburn <hwtb2@cam.ac.uk>
2020-05-28 10:26:27 +0100

    Update edit-collection template to use new Item methods

--- src/main/java/uk/cam/lib/cdl/loading/model/editor/Item.java
+++ src/main/java/uk/cam/lib/cdl/loading/model/editor/Item.java
@@ -1,5 +1,7 @@
 package uk.cam.lib.cdl.loading.model.editor;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.nio.file.Path;
 import java.util.Optional;
 
@@ -9,4 +11,13 @@ import java.util.Optional;
 public interface Item {
     Path id();
     Optional<String> fileData();
+
+    /**
+     * Get the Item's name.
+     *
+     * <p>The default implementation is to use the final portion of the {@link #id()}, without any file extension.</p>
+     */
+    default String name() {
+        return FilenameUtils.removeExtension(id().getFileName().toString());
+    }
 }
